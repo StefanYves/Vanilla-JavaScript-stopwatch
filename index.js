88cf93f04e7befe1a4ca26a4a177f013d7ea9523
StefanYves <stefannisipeanu02@gmail.com>
2022-12-07 23:56:17 +0200

    Created event handles

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ var buttonStop = document.getElementById("btnStop");
 var buttonReset = document.getElementById("btnReset");
 var appendTens = document.getElementById("tens");
 var appendSeconds = document.getElementById("seconds");
+var Interval;
+
+buttonStart.onclick = function () {
+  clearInterval(Interval);
+  Interval = setInterval(startTimer, 10);
+};
+
+buttonStop.onclick = function () {
+  clearInterval(Interval);
+};
+
+buttonReset.onclick = function () {
+  clearInterval(Interval);
+  tens = "00";
+  seconds = "00";
+  appendSeconds.innerHTML = seconds;
+  appendTens.innerHTML = tens;
+};
