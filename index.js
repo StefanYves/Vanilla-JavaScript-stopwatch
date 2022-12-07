0604a079a663bb3fccdb8cb26b2c9dd83d32b580
StefanYves <stefannisipeanu02@gmail.com>
2022-12-07 23:48:05 +0200

    Made a javascript file and introduced parameters

---
+++ index.js
@@ -0,0 +1,7 @@
+var seconds = 0;
+var tens = 0;
+var buttonStart = document.getElementById("btnStart");
+var buttonStop = document.getElementById("btnStop");
+var buttonReset = document.getElementById("btnReset");
+var appendTens = document.getElementById("tens");
+var appendSeconds = document.getElementById("seconds");
