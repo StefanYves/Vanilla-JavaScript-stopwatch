2ee870e817b4834c7bfd166166d684653a184813
StefanYves <stefannisipeanu02@gmail.com>
2022-12-08 11:02:37 +0200

    Finished the project

--- index.js
+++ index.js
@@ -23,3 +23,25 @@ buttonReset.onclick = function () {
   appendSeconds.innerHTML = seconds;
   appendTens.innerHTML = tens;
 };
+
+function startTimer() {
+  tens++;
+
+  if (tens <= 9) {
+    appendTens.innerHTML = "0" + tens;
+  }
+  if (tens > 9) {
+    appendTens.innerHTML = tens;
+  }
+
+  if (tens > 99) {
+    seconds++;
+    appendSeconds.innerHTML = "0" + seconds;
+    tens = "0";
+    appendTens.innerHTML = "0" + 0;
+  }
+
+  if (seconds > 9) {
+    appendSeconds.innerHTML = seconds;
+  }
+}
